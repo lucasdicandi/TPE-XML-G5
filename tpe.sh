2fba403f3b887e00df9d200fb2b8e4a613d9f269
lucasdicandi <lucasdicandi@gmail.com>
2024-06-09 13:24:08 -0300

    Add files via upload

--- tpe.sh
+++ tpe.sh
@@ -25,13 +25,22 @@ if ! [[ "$SERIE" =~ ^(sc|xf|cw|go|mc)$ ]]; then
 fi
 
 # Descargar los archivos XML
+echo "Descargando drivers_standings.xml..."
 curl "https://api.sportradar.com/nascar-ot3/$SERIE/$ANO/standings/drivers.xml?api_key=$API_KEY" -o drivers_standings.xml
+
+echo "Descargando drivers_list.xml..."
 curl "https://api.sportradar.com/nascar-ot3/$SERIE/$ANO/drivers/list.xml?api_key=$API_KEY" -o drivers_list.xml
 
 # Ejecutar las consultas y transformaciones XQuery y XSLT
+
+echo "Generando nascar_data.xml..."
 java net.sf.saxon.Query extract_nascar_data.xq -o:nascar_data.xml
+
+echo "Generando nascar_page.fo..."
 java net.sf.saxon.Transform -s:nascar_data.xml -xsl:generate_fo.xsl -o:nascar_page.fo
 
+
+echo "Generando nascar_report.pdf..."
 # Generar el reporte PDF usando Apache FOP
 ./fop/fop.cmd -fo nascar_page.fo -pdf nascar_report.pdf
 
